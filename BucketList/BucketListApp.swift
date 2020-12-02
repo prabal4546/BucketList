c66bb5db0b41eea6a57bbd2ed3dfc2215a150eb7
PRABAL4546 <prabaljitwalia4546@gmail.com>
2020-12-02 19:17:02 +0530

    first commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by PRABALJIT WALIA     on 02/12/20.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
