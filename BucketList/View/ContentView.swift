9767d3a43238e79583d3ae6bf6b82dc2e89f59a5
PRABAL4546 <prabaljitwalia4546@gmail.com>
2020-12-05 15:39:36 +0530

    face id/ touch id added

--- BucketList/View/ContentView.swift
+++ BucketList/View/ContentView.swift
@@ -6,13 +6,46 @@
 //
 
 import SwiftUI
+import LocalAuthentication
 
 struct ContentView: View {
     
+    @State private var isUnlocked = false
+    
+    func authenticate(){
+        let context = LAContext()
+        var error:NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error){
+            let reason = "we need to securely unlock your data."
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason, reply: {success, authenticationError in
+                DispatchQueue.main.async {
+                    if success{
+                        //if authenticated successfully
+                        self.isUnlocked = true
+                        
+                    }else{
+                        
+                    }
+                }
+            })
+        }
+        else{
+            //their is not authentication at all, like iPod touch
+        }
+    }
 
     var body: some View {
         MapView()
             .edgesIgnoringSafeArea(.all)
+        VStack{
+            if self.isUnlocked{
+                Text("Unlocked")
+            }else{
+                Text("Locked")
+            }
+        }
+        .onAppear(perform:authenticate)
     }
 }
 
