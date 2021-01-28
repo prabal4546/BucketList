cc0e7dacc987961c4838cab1290be01533a64cad
PRABAL4546 <prabaljitwalia4546@gmail.com>
2021-01-28 17:09:45 +0530

    result models

--- BucketList/Model/Result.swift
+++ BucketList/Model/Result.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+struct Result:Codable {
+    let query:Query
+}
+struct Query: Codable {
+    let pages:[Int:Page]
+}
+
+struct Page:Codable {
+    let page:Int
+    let pageid:UUID
+    let title:String
+    let terms:[String:[String]]?
+}
--- BucketList/View/EditView.swift
+++ BucketList/View/EditView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 import MapKit
 struct EditView: View {
+    
+    enum LoadingState {
+        case loading,loaded,failed
+    }
     @Environment(\.presentationMode) var presentationMode
     @ObservedObject var placemark:MKPointAnnotation
+    @State private var loadingState = LoadingState.loading
+    @State private var pages = [Page]()
     var body: some View {
         NavigationView{
             Form{
                 Section{
                     TextField("Place name", text: $placemark.wrappedTitle)
                     TextField("Description", text: $placemark.wrappedSubtitle)
+                }
+                Section(header:Text("Nearby...")){
+                    if loadingState == .loaded{
+                        List(pages, id: \.pageid){page in
+                            Text(page.title)
+                                .font(.headline)
+                            + Text(":") +
+                            Text("Page description here")
+                                .italic()
+                        }
+                        else if loadingState = .loading{
+                            Text("Loading..")
+                        }else{
+                            Text("Please check your connection and try again")
+                        }
+                    }
+                    
                 }
             }
             .navigationBarTitle(Text("Edit Place"))
