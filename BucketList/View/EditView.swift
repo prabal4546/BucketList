08a5484cfb156605e521d79a982aec74adb4a4c1
PRABAL4546 <prabaljitwalia4546@gmail.com>
2021-01-28 17:30:22 +0530

    wikipedia API results fetch

--- BucketList/View/EditView.swift
+++ BucketList/View/EditView.swift
@@ -32,7 +32,7 @@ struct EditView: View {
                             Text("Page description here")
                                 .italic()
                         }
-                        else if loadingState = .loading{
+                        if loadingState == .loading{
                             Text("Loading..")
                         }else{
                             Text("Please check your connection and try again")
@@ -45,9 +45,34 @@ struct EditView: View {
             .navigationBarItems(trailing: Button("Done"){
                 self.presentationMode.wrappedValue.dismiss()
             })
+            .onAppear(perform:fetchNearbyPlaces)
         }
     }
+    func fetchNearbyPlaces(){
+        let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(placemark.coordinate.latitude)%7C\(placemark.coordinate.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
+        guard let url = URL(string: urlString) else{
+            print("bad url")
+            return
+        }
+        URLSession.shared.dataTask(with: url){data,response,error in
+            if let data = data{
+                let decoder = JSONDecoder()
+                if let items = try? decoder.decode(Result.self, from: data){
+                    self.pages = Array(items.query.pages.values)
+                    self.loadingState = .loaded
+                    return
+                }
+            }
+            else{
+                self.loadingState = .failed
+            }
+        }.resume()
+        
+        
+
+    }
 }
+
 
 struct EditView_Previews: PreviewProvider {
     static var previews: some View {
